6f243ef61a879b56a9da1f3e4b29808edcfec22e
Viorel <zamaneagra@gmail.com>
2019-02-08 20:32:45 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,7 +26,8 @@ $(document).ready(function(){
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
+                //adding offset top - 70 px , fiindca nav este fixat top
+                scrollTop: $(hash).offset().top - 70
             }, 800, function(){
 
                 // Add hash (#) to URL when done scrolling (default click behavior)
