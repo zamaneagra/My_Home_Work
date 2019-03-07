e40602bd587b69a76808f5c64efe4a7839ef89b1
Viorel <zamaneagra@gmail.com>
2019-03-08 00:22:44 +0200

    get / put

--- Ajax/myJavaScript.js
+++ Ajax/myJavaScript.js
@@ -1,105 +1,64 @@
+////////////////////////////////////////
+// get users from external data base //
+function getUsers() {
+        for(var i = 1; i < 5; i++)
+        $.ajax({
+            url: 'https://reqres.in/api/users?page=' + i,
+            type: 'GET',
+            success: function(response){
+                for(var i =0; i < response.data.length;i++ ) {
+                    $("#user-list").append(
+                        "<li>" + response.data[i].first_name + " " + response.data[i].last_name + " (id: " + response.data[i].id + ")" + "</li>"
+                    )
+                }
+                console.log(response);
+            }
 
+        });
+}
+
+////////////////////////////////
+// add in data base new user //
+function saveUsers() {
+    var newUser = $("#firstName").val() + " " + $("#lastName").val();
+
+    $.ajax({
+        url: "https://reqres.in/api/users",
+        type: "POST",
+        data: {
+            name: newUser,
+        },
+
+        success: function(response){
+            $("#confirmUserSaved").append(
+            "<p> New user <span class='userAdded text-capitalize'>" + newUser + "</span> was added!!!</p>"
+            )
+
+        },
+
+        error: function(response) {
+            alert("EEEEEEEEERRRRRRRRROOOOOOOOOOR")
+            console.log(response);
+        }
+    });
+}
+
+/////////////////////////
+/// get user by ID  ////
 function getUser() {
     var userId = $("#userId").val();
     $.ajax({
         url: "https://reqres.in/api/users/" + userId,
         type: "GET",
-        success: function(response){
-            console.log(response);
-        }
-    });
-}
-
-
-
-function updateUser() {
-    var formData = getFormData();
-    var userId = $("#userId").val();
-
-    console.log(formData);
-    $.ajax({
-        url: "https://reqres.in/api/users/" + userId,
-        type: "PUT",
-        data: formData,
-        success: function(response){
-            console.log(response);
-        }
-    });
-}
-
-function getUsers() {
-
-    $.ajax({
-        url: "https://reqres.in/api/users?page=1",
-        type: "GET",
-        success: function(response){
-            for(var i =0; i < response.data.length;i++ ) {
-                $("#user-list").append(
-                    "<p>" + response.data[i].first_name + " " + response.data[i].last_name + "</p>"
+            success: function(response){
+                $("#getUser").append(
+                    "<p>" + response.data.first_name + " " + response.data.last_name + "</p>" + "<p>" + '<img src="' + response.data.avatar + '">' + "</p>"
                 )
             }
-            console.log(response);
-        }
-    });
-
-    $.ajax({
-        url: "https://reqres.in/api/users?page=2",
-        type: "GET",
-        success: function(response){
-            for(var i =0; i < response.data.length;i++ ) {
-                $("#user-list").append(
-                    "<p>" + response.data[i].first_name + " " + response.data[i].last_name + "</p>"
-                )
-            }
-            console.log(response);
-        }
-    });
-
-    $.ajax({
-        url: "https://reqres.in/api/users?page=3",
-        type: "GET",
-        success: function(response){
-            for(var i =0; i < response.data.length;i++ ) {
-                $("#user-list").append(
-                    "<p>" + response.data[i].first_name + " " + response.data[i].last_name + "</p>"
-                )
-            }
-            console.log(response);
-        }
-    });
-
-    $.ajax({
-        url: "https://reqres.in/api/users?page=4",
-        type: "GET",
-        success: function(response){
-            for(var i =0; i < response.data.length;i++ ) {
-                $("#user-list").append(
-                    "<p>" + response.data[i].first_name + " " + response.data[i].last_name + "</p>"
-                )
-            }
-            console.log(response);
-        }
-    });
-
-}
-
-function saveUsers() {
-    var formData = getFormData();
-    console.log(formData);
-    $.ajax({
-        url: "https://reqres.in/api/users",
-        type: "POST",
-        data: formData,
-        success: function(response){
-            console.log(response);
-        } ,
-        error: function(response) {
-            alert("EEEEEEEEERRRRRRRRROOOOOOOOOOR")
-        }
     });
 }
 
-function getFormData() {
-    return { first_name: $("#firstName").val(),
-        last_name: $("#lastName").val(), lalala: "as.dsdkjasdkasjd.ask 1232 1312 31 1"};
-}
+
+
+
+
--- js/my.js
+++ js/my.js
@@ -1,3 +1,42 @@
+var numberOfBlogs = 0;
+
+function toggleForm(){
+    var contactForm = $('#contact');
+
+    if (contactForm.is(':visible')) {
+        contactForm.slideToggle(2000);
+    }   else {
+        contactForm.slideToggle(2000);
+    }
+}
+
+function sendUserData (){
+    var formData = getFormData();
+
+    $.ajax({url: "https://reqres.in/api/users",
+        type: 'POST',
+        data: formData,
+        success: function(result){
+            console.log("success");
+        },
+        error: function(result) {
+            console.log("fail");
+        }
+    });
+}
+
+function getFormData(){
+    var unindexed_array = $('form').serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+        indexed_array[n['name']] = n['value'];
+    });
+    return indexed_array;
+}
+
+
+// document on ready //
 $(function(){
         /// changing bkg color //
     $('#changeColor').click(function(){
@@ -21,5 +60,15 @@ $(function(){
     $('.portDiv').on('mouseleave', function () {
         $(this).removeClass('clip-me');
     });
+
+    /// form hide - unhide //
+    $('#blog-read-text').hide();
+    $('#contact').hide();
+    /// input form hide-unhide //
+    numberOfBlogs = $('#blog .row') [0].childElementCount;
+    console.log(numberOfBlogs);
+    if (numberOfBlogs >= 6) {
+        $('#blog-read-text').show();
+    }
 
 });
