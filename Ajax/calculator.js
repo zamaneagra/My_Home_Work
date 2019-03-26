39c231cca03552523282a7ace1220a16698bc8f8
Viorel <zamaneagra@gmail.com>
2019-03-26 14:02:39 +0200

    some changes

--- Ajax/calculator.js
+++ Ajax/calculator.js
@@ -59,7 +59,43 @@ window.onload = getSliderLoan;
 
 
 
+////////////////////////////////////////////////////////////////
+///////////////  CALCULATOR for your bill ////////////////////////
 
+$(function () {
+    //Hide the tip amount on load
+    $('#each').hide();
+    $('#totalTip').hide();
+});
+
+function calculateTip() {
+    var billAmt = $('#billamt').val();
+    var serviceQual = $('#serviceQual').val();
+    var numOfPeople = $('#peopleamt').val();
+
+    //validate input
+    if (billAmt === "" || serviceQual == 0) {
+        alert("Please enter values");
+        return;
+    }
+    //Check to see if this input is empty or less than or equal to 1
+    if (numOfPeople === "" || numOfPeople <= 1) {
+        numOfPeople = 1;
+        $('#each').hide();
+    } else {
+        $('#each').css('display', 'block');
+    }
+
+    //Calculate tip
+    var total = (billAmt * serviceQual) / numOfPeople;
+    //round to two decimal places
+    total = Math.round(total * 100) / 100;
+    //next line allows us to always have two digits after decimal point
+    total = total.toFixed(2);
+    //Display the tip
+    $('#totalTip').css('display', 'block');
+    $("#tip").html(total);
+}
 
 
 
